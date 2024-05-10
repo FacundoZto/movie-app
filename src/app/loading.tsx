ac1e1752a803b4a2d8e5b6b598672315db0fd370
facundozto <facundozerbato@outlook.com>
2024-05-10 12:16:42 -0300

    Add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+
+const loading = () => {
+ 
+  return (
+    <div className="flex justify-center mt-4">
+      <img src="spinner.svg" alt="Loading..." />
+    </div>
+  )
+}
+
+export default loading
